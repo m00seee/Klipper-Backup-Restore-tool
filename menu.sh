762415c39921dad20b10f7f73acc025e68bab75f
m00se <housam8@gmail.com>
2026-05-12 20:25:51 +1000

    Fix backspace over SSH by normalising terminal with stty sane

--- menu.sh
+++ menu.sh
@@ -8,6 +8,9 @@
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'
 BLUE='\033[0;34m'; CYAN='\033[0;36m'; BOLD='\033[1m'; NC='\033[0m'
 
+# Normalise terminal so backspace works correctly over SSH
+[[ -t 0 ]] && stty sane 2>/dev/null
+
 CONFIG_FILE="$HOME/.klipper_backup.conf"
 KLIPPER_CONFIG="$HOME/printer_data/config"
 KLIPPER_EXTRAS="$HOME/klipper/klippy/extras"
